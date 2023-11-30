c437527825026c9f01a04e77c9d1c1cc819b94ff
Michael Apau <86720214+Data4Dev21@users.noreply.github.com>
2023-11-30 15:10:29 +0000

    Preppin_Data_2023_week_6.sql

---
+++ Preppin_Data_2023_week_6.sql
@@ -0,0 +1,58 @@
+/*Requirements
+Reshape the data so we have 5 rows for each customer, with responses for the Mobile App and Online Interface being in separate fields on the same row
+Clean the question categories so they don't have the platform in from of them
+e.g. Mobile App - Ease of Use should be simply Ease of Use
+Exclude the Overall Ratings, these were incorrectly calculated by the system
+Calculate the Average Ratings for each platform for each customer 
+Calculate the difference in Average Rating between Mobile App and Online Interface for each customer
+Catergorise customers as being:
+Mobile App Superfans if the difference is greater than or equal to 2 in the Mobile App's favour
+Mobile App Fans if difference >= 1
+Online Interface Fan
+Online Interface Superfan
+Neutral if difference is between 0 and 1
+Calculate the Percent of Total customers in each category, rounded to 1 decimal place*/
+
+WITH cte AS
+(
+SELECT customer_id
+      ,split_part(mobile_ratings, '___', 1) AS platform
+      ,split_part(mobile_ratings, '___', 2) AS method
+      ,scale
+    FROM 
+(SELECT *
+    FROM TIL_PLAYGROUND.PREPPIN_DATA_INPUTS.PD2023_WK06_DSB_CUSTOMER_SURVEY) AS src--first source table
+    UNPIVOT(scale FOR mobile_ratings IN (MOBILE_APP___EASE_OF_USE, MOBILE_APP___EASE_OF_ACCESS, MOBILE_APP___NAVIGATION, MOBILE_APP___LIKELIHOOD_TO_RECOMMEND, MOBILE_APP___OVERALL_RATING,
+                                         ONLINE_INTERFACE___EASE_OF_USE, ONLINE_INTERFACE___EASE_OF_ACCESS, ONLINE_INTERFACE___NAVIGATION, ONLINE_INTERFACE___LIKELIHOOD_TO_RECOMMEND,
+	                                       ONLINE_INTERFACE___OVERALL_RATING  --data type relevant here
+)) AS pvt -- first pivot
+)
+,cte1 AS
+(
+SELECT *
+    FROM cte
+    pivot(sum(scale)FOR platform IN ('MOBILE_APP', 'ONLINE_INTERFACE'))--second pivot
+    WHERE method!='OVERALL_RATING'
+)
+, cte2 AS
+(
+SELECT customer_id
+      ,avg("'MOBILE_APP'") AS mobile_avg
+      ,avg("'ONLINE_INTERFACE'") AS online_avg
+      ,mobile_avg-online_avg AS rating_variance,
+      CASE
+      WHEN rating_variance >=2 THEN 'mobile_app_superfans'
+      WHEN rating_variance >=1 THEN 'mobile_app_fans'
+      WHEN rating_variance <=-2 THEN 'online_superfans'
+      WHEN rating_variance <=-1 THEN 'online_fans'
+      ELSE 'neutral' END AS fan_category
+    FROM cte1
+    --WHERE customer_id=535084
+    GROUP BY 1
+    )
+    SELECT fan_category
+          ,count(customer_id) AS customer_per_category
+          ,(SELECT count(DISTINCT customer_id) FROM cte2) AS total_customers
+          ,round(customer_per_category/total_customers*100,1) AS customer_percentage
+          FROM cte2
+          GROUP BY 1;
